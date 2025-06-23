a0eb7a5c750c80999b6bee5a30981e966206e572
smrtj <jordan@smrtpayments.com>
2025-06-23 06:01:36 +0000

    clean up

--- tmp_ebike_tmp/ebike/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "[+] Installing dependencies..."
-npm install express body-parser pdfkit googleapis nodemailer axios dotenv
-
-echo "[+] Starting the server on port 3000..."
-node server.js
